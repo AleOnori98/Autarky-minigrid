b4436b78ffe6f31b58ff9493f7485a67507b1056
Alessandro Onori <onorialessandro1998@gmail.com>
2025-06-26 01:10:00 +0200

    Update post_processing.jl

--- backend/models/linear/post_processing.jl
+++ backend/models/linear/post_processing.jl
@@ -4,7 +4,6 @@ using JuMP, JSON3, Dates, Statistics
 include(joinpath(@__DIR__, "utils.jl"))
 using .Utils: import_time_series
 
-# Safe accessor for optional JuMP variables
 function safe_value(model::Model, var::Symbol)
     return haskey(model, var) ? value(model[var]) : 0.0
 end
@@ -20,22 +19,15 @@ function write_results_to_json(
 )
     results = Dict()
 
-    # === Meta Info ===
     results["timestamp"] = string(Dates.now())
     results["project_id"] = project_id
 
-    # === Flags ===
     enabled = system_config["enabled_components"]
     has_solar = enabled["solar_pv"]
-    has_wind = enabled["wind_turbine"]
     has_battery = enabled["battery"]
     has_generator = enabled["diesel_generator"]
     has_grid = enabled["grid_connection"]
-    if has_grid
-        allow_export = get(system_config["grid_connection"], "allow_export", false)
-    else
-        allow_export = false
-    end
+    allow_export = has_grid ? get(system_config["grid_connection"], "allow_export", false) : false
 
     project_settings = project_setup["project_settings"]
     project_lifetime = project_settings["time_horizon"]
@@ -47,19 +39,17 @@ function write_results_to_json(
     currency = tech_params["economic_settings"]["currency"]
     discount_factors = [1 / ((1 + discount) ^ y) for y in 1:project_lifetime]
 
-    # === Load Time Series ===
     ts_dir = joinpath("projects", project_id, "time_series")
     load = import_time_series(joinpath(ts_dir, "load_demand.csv"), num_seasons, true)
 
-    # === Sizing ===
-    sizing = Dict(
+    # === Sizing
+    results["sizing"] = Dict(
         "solar_kw" => has_solar ? safe_value(model, :solar_units) * res_potential["solar_pv"]["nominal_capacity"] : 0.0,
         "battery_kwh" => has_battery ? safe_value(model, :battery_units) * tech_params["technology_parameters"]["battery"]["nominal_capacity"] : 0.0,
         "generator_kw" => has_generator ? safe_value(model, :generator_units) * tech_params["technology_parameters"]["diesel_generator"]["nominal_capacity"] : 0.0
     )
-    results["sizing"] = sizing
 
-    # === Costs ===
+    # === Costs
     results["costs"] = Dict(
         "NPC[kUSD]" => round(safe_value(model, :NPC) / 1000, digits=2),
         "CAPEX[kUSD]" => round(safe_value(model, :CAPEX) / 1000, digits=2),
@@ -69,26 +59,24 @@ function write_results_to_json(
         "Salvage[kUSD]" => round(safe_value(model, :Salvage_npv) / 1000, digits=2)
     )
 
-    # === LCOE ===
+    # === LCOE
     demand_sum = sum(sum(season_weights[s] * load[t, s] for t in 1:T, s in 1:num_seasons) * discount_factors[y] for y in 1:project_lifetime)
     results["LCOE[USD/kWh]"] = round(safe_value(model, :NPC) / demand_sum, digits=3)
 
-    # === Operation KPIs ===
+    # === Operation KPIs
     op = Dict(
         "solar[MWh]" => 0.0,
         "generator[MWh]" => 0.0,
         "fuel_liters" => 0.0,
         "battery_charge[MWh]" => 0.0,
         "battery_discharge[MWh]" => 0.0,
-        "grid_import_[MWh]" => 0.0,
+        "grid_import[MWh]" => 0.0,
         "grid_export[MWh]" => 0.0,
         "solar_curtailment" => 0.0,
         "lost_load" => 0.0
     )
-
     if has_solar
-        solar_prod = sum(season_weights[s] * value(model[:solar_production][t,s]) for t in 1:T, s in 1:num_seasons)
-        op["solar[MWh]"] = round(solar_prod / 1000, digits=2)
+        op["solar[MWh]"] = round(sum(season_weights[s] * value(model[:solar_production][t,s]) for t in 1:T, s in 1:num_seasons) / 1000, digits=2)
     end
     if has_generator
         gen = sum(season_weights[s] * value(model[:generator_production][t,s]) for t in 1:T, s in 1:num_seasons)
@@ -112,34 +100,33 @@ function write_results_to_json(
     end
     results["operation"] = op
 
-    # === Dispatch ===
+    # === Dispatch
     dispatch = Dict{String, Any}()
     for s in 1:num_seasons
         data = Dict(
             "timestep" => collect(1:T),
-            "load[kWh]" => load[:, s],
-            "solar[kWh]" => fill(0.0, T),
-            "charge[kWh]" => fill(0.0, T),
-            "discharge[kWh]" => fill(0.0, T),
-            "soc[kWh]" => fill(0.0, T),
-            "generator[kWh]" => fill(0.0, T),
-            "grid_import[kWh]" => fill(0.0, T),
-            "grid_export[kWh]" => fill(0.0, T),
-            "solar_curtailment[kWh]" => fill(0.0, T),
-            "lost_load[kWh]" => fill(0.0, T)
+            "Load Demand (kWh)" => load[:, s],
+            "Solar Production (kWh)" => fill(0.0, T),
+            "Battery Charge (kWh)" => fill(0.0, T),
+            "Battery Discharge (kWh)" => fill(0.0, T),
+            "State of Charge (kWh)" => fill(0.0, T),
+            "Generator Production (kWh)" => fill(0.0, T),
+            "Grid Import (kWh)" => fill(0.0, T),
+            "Grid Export (kWh)" => fill(0.0, T),
+            "Solar Curtailment (kWh)" => fill(0.0, T),
+            "Lost Load (kWh)" => fill(0.0, T)
         )
-        if has_solar data["solar[kWh]"] = value.(model[:solar_production])[:, s] end
+        if has_solar data["Solar Production (kWh)"] = value.(model[:solar_production])[:, s] end
         if has_battery
-            data["charge[kWh]"] = value.(model[:battery_charge])[:, s]
-            data["discharge[kWh]"] = value.(model[:battery_discharge])[:, s]
-            data["soc[kWh]"] = value.(model[:SOC])[:, s]
+            data["Battery Charge (kWh)"] = value.(model[:battery_charge])[:, s]
+            data["Battery Discharge (kWh)"] = value.(model[:battery_discharge])[:, s]
+            data["State of Charge (kWh)"] = value.(model[:SOC])[:, s]
         end
-        if has_generator data["generator[kWh]"] = value.(model[:generator_production])[:, s] end
+        if has_generator data["Generator Production (kWh)"] = value.(model[:generator_production])[:, s] end
         if has_grid
-            data["grid_import[kWh]"] = value.(model[:grid_import])[:, s]
-            if allow_export data["grid_export[kWh]"] = value.(model[:grid_export])[:, s] end
+            data["Grid Import (kWh)"] = value.(model[:grid_import])[:, s]
+            if allow_export data["Grid Export (kWh)"] = value.(model[:grid_export])[:, s] end
         end
-        # Add if you implement curtailment/lost load in the model
         dispatch["season_$(s)"] = data
     end
     results["dispatch"] = dispatch
